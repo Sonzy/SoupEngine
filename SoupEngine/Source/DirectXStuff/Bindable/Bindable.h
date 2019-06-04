1c9f7b71cad38b756c197bac51e698487af26513
Sonzy <sonzeeeeeeeeee@hotmail.com>
2019-06-05 00:14:24 +0100

    More organising stuff

---
+++ SoupEngine/Source/DirectXStuff/Bindable/Bindable.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "DirectXStuff/Graphics.h"
+
+class Bindable
+{
+public:
+	virtual void Bind(Graphics& gfx) noexcept = 0;
+	virtual ~Bindable() = default;
+protected:
+	static ID3D11DeviceContext* GetContext(Graphics& gfx) noexcept;
+	static ID3D11Device* GetDevice(Graphics& gfx) noexcept;
+	static DXGIInfoManager& GetInfoManager(Graphics& gfx);
+	//TODO: 6 mins into bindables
+};
